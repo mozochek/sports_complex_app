6df72a3a372c2b747c370d5a7ba570097fa2325a
Fokin Kirill <mozochek@gmail.com>
2021-03-14 17:46:08 +0300

    Create new widget

---
+++ lib/src/presentation/workouts/widgets/workouts_date_picker_line.dart
@@ -0,0 +1,25 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutsDatePickerLine extends StatelessWidget {
+  const WorkoutsDatePickerLine({
+    required this.onDateChange,
+    this.initialSelectedDate,
+    Key? key,
+  }) : super(key: key);
+
+  final DateTime? initialSelectedDate;
+  final Function(DateTime) onDateChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return DatePicker(
+      DateTime.now(),
+      daysCount: 14,
+      // TODO: check when null provided
+      initialSelectedDate: initialSelectedDate ?? DateTime.now(),
+      onDateChange: onDateChange,
+      locale: 'ru_RU',
+    );
+  }
+}
