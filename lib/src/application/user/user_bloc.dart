fe8c1ace2231068363302d18a783950f9e49842a
Fokin Kirill <mozochek@gmail.com>
2021-03-14 17:54:40 +0300

    Add interface implementation

--- lib/src/application/user/user_bloc.dart
+++ lib/src/application/user/user_bloc.dart
@@ -1,35 +1,68 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:sports_complex_app/injection.dart';
+import 'package:sports_complex_app/src/application/user/i_user_bloc.dart';
 
 import 'package:sports_complex_app/src/domain/user/user.dart';
 import 'package:sports_complex_app/src/domain/user/user_state.dart';
+import 'package:sports_complex_app/src/presentation/router/i_sports_complex_router.dart';
 
 @LazySingleton(
+  as: IUserBloc,
   env: [
     Environment.dev,
     Environment.prod,
     Environment.test,
   ],
 )
-class UserBloc {
-  final _userController = BehaviorSubject<User>();
+class UserBloc implements IUserBloc {
+  UserBloc() {
+    _listenUserStates();
+  }
+
+  final _userController = BehaviorSubject<User?>();
 
   final StreamController<UserState> _userStateController =
       StreamController<UserState>.broadcast();
 
-  User get currentUser => _userController.value;
+  @override
+  User? get currentUser => _userController.value;
 
-  Stream<User> get userChanges => _userController.stream;
-
-  Stream<UserState> get userStateChanges => _userStateController.stream;
-
+  @override
   void addUserState(UserState state) => _userStateController.add(state);
 
-  void addUser(User user) => _userController.add(user);
+  @override
+  void addUser(User? user) => _userController.add(user);
 
+  void _listenUserStates() {
+    _userStateController.stream.listen(
+      (userState) async {
+        final router = getIt<ISportsComplexRouter>();
+        switch (userState) {
+          case UserState.signedUp:
+            await router.pushAndRemoveUntil(
+              ScreenRoutes.signIn,
+              predicate: (route) => route.settings.name == '/',
+            );
+            break;
+          case UserState.signedIn:
+            await router.pushAndRemoveUntil(
+              ScreenRoutes.bottomNav,
+            );
+            break;
+          case UserState.signedOut:
+            await router.pushAndRemoveUntil(
+              ScreenRoutes.welcome,
+            );
+            break;
+        }
+      },
+    );
+  }
+
+  @override
   Future<void> dispose() async {
     await _userStateController.close();
     await _userController.close();
