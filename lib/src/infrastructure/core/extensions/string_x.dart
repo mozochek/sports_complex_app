a532aeaf54c214354fe60fa72e640459e7b20fe2
Fokin Kirill <mozochek@gmail.com>
2021-03-14 17:53:03 +0300

    Add new helper method

--- lib/src/infrastructure/core/extensions/string_x.dart
+++ lib/src/infrastructure/core/extensions/string_x.dart
@@ -1,6 +1,23 @@
+import 'package:sports_complex_app/src/domain/user/user.dart';
+
 extension StringX on String {
-  bool get isEmailValid => RegExp(
+  bool get isEmailValid =>
+      RegExp(
           r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
-          )
-      .hasMatch(this);
+      )
+          .hasMatch(this);
+
+  UserRole get asUserRole {
+    switch (this) {
+      case 'user':
+        return UserRole.user;
+      case 'moderator':
+        return UserRole.moderator;
+      case 'admin':
+        return UserRole.admin;
+      case 'unknown':
+      default:
+        return UserRole.unknown;
+    }
+  }
 }
