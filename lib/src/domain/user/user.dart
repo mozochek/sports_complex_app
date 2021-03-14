d730294b1fabba62e269862fde8c8537643d9ef4
Fokin Kirill <mozochek@gmail.com>
2021-03-14 17:52:51 +0300

    Rework user entity

--- lib/src/domain/user/user.dart
+++ lib/src/domain/user/user.dart
@@ -1,49 +1,56 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uuid/uuid.dart';
 
+// TODO: move all classes to their own files
+// TODO: add json serializable
 class User {
   User._({
-    this.id,
-    this.data,
+    required this.id,
+    required this.additionalInfo,
+    required this.authData,
+    required this.role,
   });
 
-  factory User.fromUserData(UserData data) => User._(
-        id: Uuid().v4(),
-        data: data,
+  factory User.empty() => User._(
+        id: const Uuid().v4(),
+        additionalInfo: UserAdditionalInfo.empty(),
+        authData: UserAuthData.empty(),
+        role: UserRole.user,
+      );
+
+  factory User.fromData({
+    required UserAdditionalInfo additionalInfo,
+    required UserAuthData authData,
+    required UserRole role,
+  }) =>
+      User._(
+        id: const Uuid().v4(),
+        additionalInfo: additionalInfo,
+        authData: authData,
+        role: role,
       );
 
   final String id;
-  final UserData data;
-
-  String get fullName =>
-      '${data.additionalInfo.surname} ${data.additionalInfo.name}';
-
-  @override
-  String toString() => '$runtimeType(id: $id, data: $data)';
-}
-
-class UserData {
-  UserData({
-    @required this.additionalInfo,
-    @required this.authData,
-  });
-
   final UserAdditionalInfo additionalInfo;
   final UserAuthData authData;
+  UserRole role;
 
-  @override
-  String toString() =>
-      '$runtimeType(additionalInfo: $additionalInfo, authData: $authData)';
+  String get fullName => '${additionalInfo.surname} ${additionalInfo.name}';
 }
 
 class UserAuthData {
   UserAuthData({
-    @required this.email,
-    @required this.password,
+    required this.email,
+    required this.password,
   });
 
-  final String email;
-  final String password;
+  factory UserAuthData.empty() => UserAuthData(
+        email: '',
+        password: '',
+      );
+
+  String email;
+  String password;
 
   @override
   String toString() => '$runtimeType(email: $email, password: $password)';
@@ -51,13 +58,52 @@ class UserAuthData {
 
 class UserAdditionalInfo {
   UserAdditionalInfo({
-    @required this.surname,
-    @required this.name,
+    required this.surname,
+    required this.name,
   });
 
-  final String surname;
-  final String name;
+  factory UserAdditionalInfo.empty() => UserAdditionalInfo(
+        surname: '',
+        name: '',
+      );
+
+  factory UserAdditionalInfo.fromJson(Map<String, dynamic> json) =>
+      UserAdditionalInfo(
+        surname: json['surname'] as String,
+        name: json['name'] as String,
+      );
+
+  factory UserAdditionalInfo.fromFirestore(DocumentSnapshot doc) =>
+      UserAdditionalInfo(
+        surname: doc.data()!['surname'] as String,
+        name: doc.data()!['name'] as String,
+      );
+
+  String surname;
+  String name;
 
   @override
   String toString() => '$runtimeType(surname: $surname, name: $name)';
+}
+
+enum UserRole {
+  unknown,
+  user,
+  moderator,
+  admin,
+}
+
+extension UserRoleX on UserRole {
+  String get asString {
+    switch (this) {
+      case UserRole.unknown:
+        return 'unknown';
+      case UserRole.user:
+        return 'user';
+      case UserRole.moderator:
+        return 'moderator';
+      case UserRole.admin:
+        return 'admin';
+    }
+  }
 }
