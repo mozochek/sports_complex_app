d80646b0d1958f6b388c401ac198f7a797dfd4bd
Fokin Kirill <mozochek@gmail.com>
2021-03-14 18:03:09 +0300

    Minor changes and add doc

--- lib/src/infrastructure/auth/sign_up_exception_code.dart
+++ lib/src/infrastructure/auth/sign_up_exception_code.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:sports_complex_app/src/domain/core/exceptions/sign_up_exception.dart';
 import 'package:sports_complex_app/src/infrastructure/core/exceptions/user_repository_exception.dart';
 
@@ -21,15 +22,12 @@ enum SignUpExceptionCode {
   /// Equivalent to [FirebaseAuthException] with code 'unknown'
   firebaseUnknownCode,
 
-  // TODO: add doc
-  ///
+  /// Equivalent to [UserRepositoryException] with code [UserRepositoryException.invalidUserSurname]
   invalidUserSurname,
 
-  // TODO: add doc
-  ///
+  /// Equivalent to [UserRepositoryException] with code [UserRepositoryException.invalidUserName]
   invalidUserName,
 
-  // TODO: add doc
   /// Equivalent to [FirebaseAuthException] with code 'invalid-email'
   /// or to [UserRepositoryException] with code [UserRepositoryException.invalidEmail]
   invalidEmail,
@@ -44,31 +42,22 @@ extension SignUpExceptionCodeX on SignUpExceptionCode {
     switch (this) {
       case SignUpExceptionCode.firebaseEmailAlreadyInUse:
         return 'Данный адрес эл.почты уже зарегестрирован';
-        break;
       case SignUpExceptionCode.firebaseOperationNotAllowed:
         return 'Данный вид регистрации временно недоступен. Попробуйте позже';
-        break;
       case SignUpExceptionCode.firebaseWeakPassword:
         return 'Введен слабый пароль';
-        break;
       case SignUpExceptionCode.firebaseUnknownCode:
         return 'Введены некорректные данные';
-        break;
       case SignUpExceptionCode.invalidUserSurname:
         return 'Фамилия должна состоять из 2 и более символов';
-        break;
       case SignUpExceptionCode.invalidUserName:
         return 'Имя должно состоять из 2 и более символов';
-        break;
       case SignUpExceptionCode.invalidEmail:
         return 'Введен некорректный адрес эл.почты';
-        break;
       case SignUpExceptionCode.unsupportedCode:
         return 'Произошла неподдерживаемя ошибка';
-        break;
       default:
         return 'Произошла неизвестная ошибка';
-        break;
     }
   }
 }
