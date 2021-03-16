f19d9b9be8e208fe706e507a786687138dab9cf1
Fokin Kirill <mozochek@gmail.com>
2021-03-16 17:52:21 +0300

    Create new widget

---
+++ lib/src/presentation/profile/admin_console/halls/halls_screen/widgets/list_of_halls.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:provider/provider.dart';
+
+import 'package:sports_complex_app/generated/l10n.dart';
+import 'package:sports_complex_app/src/application/halls/actor_bloc/i_hall_actor_bloc.dart';
+import 'package:sports_complex_app/src/application/halls/form_bloc/hall_form_bloc.dart';
+import 'package:sports_complex_app/src/application/halls/form_bloc/i_hall_form_bloc.dart';
+import 'package:sports_complex_app/src/domain/halls/hall.dart';
+import 'package:sports_complex_app/src/presentation/common/delete_dialog.dart';
+import 'package:sports_complex_app/src/presentation/common/hall_list_tile_widget.dart';
+import 'package:sports_complex_app/src/presentation/profile/admin_console/halls/hall_form/hall_form.dart';
+
+class ListOfHalls extends StatelessWidget {
+  const ListOfHalls({
+    required this.halls,
+    Key? key,
+  }) : super(key: key);
+
+  final List<Hall> halls;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: halls.length,
+      itemBuilder: (_, index) {
+        final hall = halls[index];
+        return Slidable(
+          actionPane: const SlidableScrollActionPane(),
+          secondaryActions: <Widget>[
+            IconSlideAction(
+              color: Colors.yellow,
+              icon: Icons.edit,
+              onTap: () async {
+                // TODO: old navigation
+                await Navigator.of(context).push<void>(
+                  MaterialPageRoute(
+                    fullscreenDialog: true,
+                    builder: (_) => Provider<IHallFormBloc>(
+                      create: (_) => HallFormBloc.forEditing(hall),
+                      dispose: (_, bloc) async => bloc.dispose(),
+                      child: const HallForm(),
+                    ),
+                  ),
+                );
+              },
+            ),
+            IconSlideAction(
+              color: Colors.red,
+              icon: Icons.delete,
+              onTap: () async {
+                final isDeleteConfirmed = await showDialog<bool>(
+                  context: context,
+                  builder: (_) {
+                    return DeleteDialog(
+                      title: S.current.hall_delete_dialog_title,
+                      content: S.current.hall_delete_dialog_content,
+                    );
+                  },
+                );
+
+                // Null check in this statement work when user tap outside of dialog.
+                // In this case we didn't get any response from dialog so as default
+                // we set it to false
+                if (isDeleteConfirmed ?? false) {
+                  await Provider.of<IHallsActorBloc>(
+                    context,
+                    listen: false,
+                  ).delete(hall);
+                }
+              },
+            ),
+          ],
+          child: HallListTile(
+            hall: hall,
+            onTap: () async {
+              // TODO: old navigation
+              await Navigator.of(context).push<void>(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (_) => Provider<IHallFormBloc>(
+                    create: (_) => HallFormBloc.forEditing(hall),
+                    dispose: (_, bloc) async => bloc.dispose(),
+                    child: const HallForm(),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
