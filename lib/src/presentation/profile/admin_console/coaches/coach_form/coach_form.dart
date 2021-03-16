de5ef7c95363bfa5b60d0c71d64c730db757c1f4
Fokin Kirill <mozochek@gmail.com>
2021-03-16 18:02:16 +0300

    Delete unused object reference

--- lib/src/presentation/profile/admin_console/coaches/coach_form/coach_form.dart
+++ lib/src/presentation/profile/admin_console/coaches/coach_form/coach_form.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:sports_complex_app/src/application/coaches/form_bloc/i_coach_form_bloc.dart';
-import 'package:sports_complex_app/src/domain/coaches/coach.dart';
 import 'package:sports_complex_app/src/presentation/common/debug_dialog.dart';
 import 'package:sports_complex_app/src/presentation/common/scaffold_wrapper.dart';
 import 'package:sports_complex_app/src/presentation/profile/admin_console/coaches/coach_form/widgets/coach_name_text_field_widget.dart';
@@ -12,10 +11,7 @@ import 'package:sports_complex_app/src/presentation/profile/admin_console/coache
 class CoachForm extends StatelessWidget {
   const CoachForm({
     Key? key,
-    this.editedCoach,
   }) : super(key: key);
-
-  final Coach? editedCoach;
 
   @override
   Widget build(BuildContext context) {
