5d164e9493394364b7d48e7b07e5a44f1a15ca91
Fokin Kirill <mozochek@gmail.com>
2021-03-17 16:09:16 +0300

    New extension method

--- lib/src/infrastructure/core/extensions/time_of_day_x.dart
+++ lib/src/infrastructure/core/extensions/time_of_day_x.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 extension TimeOfDayX on TimeOfDay {
-  String get asFormattedString => '$hour:${minute < 10 ? '0$minute' : '$minute'}';
+  String get asFormattedString =>
+      '$hour:${minute < 10 ? '0$minute' : '$minute'}';
 
   Map<String, dynamic> toJson() => <String, dynamic>{
-    'hour': hour,
-    'minute': minute,
-  };
+        'hour': hour,
+        'minute': minute,
+      };
+
+  TimeOfDay subtract(TimeOfDay other) {
+    var resultHour = hour - other.hour;
+    var resultMinute = minute - other.minute;
+
+    if (minute < other.minute) {
+      resultHour--;
+      resultMinute = other.minute - minute;
+    }
+
+    return TimeOfDay(
+      hour: resultHour,
+      minute: resultMinute,
+    );
+  }
 }
